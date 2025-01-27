4a0c4826be41169c980d5438cf0139a624f96c33
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2025-01-27 16:43:09 -0500

    updates to datamodel

--- src/datamodel.ts
+++ src/datamodel.ts
@@ -1,47 +1,20 @@
 import type {
   Edge, Node,
   Node as XyzFlowNode,
-  NodeProps, NodeTypes
+  NodeProps,
+  NodeTypes
 } from "@xyflow/react";
 
 
-export type FlowNodePosition = {
-  x: number;
-  y: number;
-  z?: number;
-}
-
-export type StoreNodeConfig = {
-  id: string,
-  type: string,
-  position: FlowNodePosition,
-  data: StoreNodeType
-}
-
-export type BigraphNodeType = NodeType;
-
-export type ProcessNodeType = BigraphNodeType;
-
-export type StepNodeType = BigraphNodeType;
-
-export type CompositionType = {
-  [key: string]: BigraphNodeType | ProcessNodeType | StepNodeType;
-}
-
-export type BigraphFlowNodeType = XyzFlowNode<ProcessNodeType> | XyzFlowNode<StepNodeType> | XyzFlowNode<BigraphNodeSpecType>;
-
-export type BigraphNodeSpecType = Omit<BigraphNodeType, "nodeId">;
-
-export type StoreNodeType = {
-  value: any;
-  connections: ConnectionType[];
-} & {
-  [key: string]: any;
-}
+/* base models for nodes and stores */
 
 // base node type
-export type NodeType = {
-  nodeId: string;
+export type BaseNode = {
+  nodeId: string
+}
+
+// base type for process and step nodes
+export type BigraphNode =  BaseNode & {
   _type: string;
   address: string;
   config: Record<string, any>;
@@ -51,55 +24,76 @@ export type NodeType = {
   [key: string]: any; // allows indexing with a string
 };
 
-// config attribute
-export type ConfigType = {
+export type ProcessNode = BigraphNode;
+
+export type StepNode = BigraphNode;
+
+// data stores (input and output ports)
+export type StoreNode = BaseNode & {
+  value: string,
+  connections?: string[]  // this field is used to keep track of node-store connection-state
+} & {
   [key: string]: any;
 }
 
-// ports (to be verified)
-export type PortType = {
-  name: string;
-  store: StoreType;
-} & {
-  [key: string]: any; // allows indexing with a string
-};
-
-export type StoreType = {
-  value: any;
-  connections: Connection[];
-} & {
-  [key: string]: any;  // allows indexing with a string
+// overall composition type representing the "document" that is given to Composite(config={'state': ...})
+export type Composition = {
+  [key: string]: BigraphNode | ProcessNode | StepNode;
 }
-
-export type Connection = {
-  nodeId: string;
-  direction: DirectionType | string;
-};
-
-export type ConnectionType = {
-  nodeId: string;
-  direction: DirectionType | string;
-}
-
-export type DirectionType = "in" | "out";
-
 
 // used for indexing node dynamically in event listeners
-export type NodeKeyType = "_type" | "address" | "inputs" | "outputs" | "config" | "nodeId";
+export type BigraphNodeKey = keyof BigraphNode | keyof ProcessNode | keyof StepNode;
+
+export type StoreNodeKey = keyof StoreNode;
 
 
-// primary base flow node interface fulfillment type
-export type FlowNodeType = XyzFlowNode<NodeType> | XyzFlowNode<NodeSpecType>;
+/* react-flow-specific interface fulfillment */
+
+// react flow-specific interface fulfillment for process and step nodes
+export type BigraphFlowNode = XyzFlowNode<ProcessNode> | XyzFlowNode<StepNode>;
+
+// react flow-specific interface fulfillment for data stores
+export type StoreFlowNode = XyzFlowNode<StepNode>;
+
+// high-level type for easily/iteratively getting react flow-specific data
+export type BaseFlowNodeConfig = {
+  id: string,
+  type: string,
+  position: {
+    x: number,
+    y: number
+  },
+  data: StoreNode | ProcessNode | StepNode | BigraphNode
+}
+
+export type ProcessFlowNodeConfig = BaseFlowNodeConfig & {
+  data: ProcessNode
+}
+
+export type StepFlowNodeConfigType = BaseFlowNodeConfig & {
+  data: StepNode
+}
+
+export type StoreFlowNodeConfigType = BaseFlowNodeConfig & {
+  data: StoreNode
+}
+
+/* spec models consumed by the client */
+
+// process/step/store spec types with nodeId omitted (in the proper format for export)
+export type ExportedNode = Omit<BigraphNode | StepNode | ProcessNode | StoreNode, "nodeId">;
+
+export type ExportedBigraphNode = ExportedNode;
+
+export type ExportedStoreNode = ExportedNode;
 
 
 /*** bigraph edges (mostly cosmetic) ***/
 type ButtonEdgeData = {};
 
-type DataEdgeData = {};
+export type ButtonEdge = Edge<ButtonEdgeData>;
 
-export type ButtonEdge = Edge<ButtonEdgeData> | Edge<DataEdgeData> | Edge<DataEdgeType>;
-
-export type DataEdgeType<T extends Node = Node> = Edge<{
+export type DataEdge<T extends Node = Node> = Edge<{
   /**
    * The key to lookup in the source node's `data` object. For additional safety,
    * you can parameterize the `DataEdge` over the type of one of your nodes to
@@ -117,42 +111,6 @@ export type DataEdgeType<T extends Node = Node> = Edge<{
    */
   path?: "bezier" | "smoothstep" | "step" | "straight";
 }>;
-
-// consumed by the client
-export type SpecType = Record<string, any>;
-
-export type PortSpecType = Record<string, string[]>;
-
-export type NodeSpecType = Omit<NodeType, "nodeId">;
-
-export type StateSpecType = {
-  [key: string]: NodeType | NodeSpecType | string | number;
-}
-
-export type CompositeSpecType = {
-  state: StateSpecType;
-  composition: string;
-}
-
-export type AppNode = {
-  id: string,
-  type: string,
-  position: {
-    x: number,
-    y: number
-  },
-  data: any
-};
-
-export type AppEdge = {
-  id: string,
-  type: string,
-  source: string,
-  target: string,
-  animated: boolean,
-};
-
-
 
 
 
